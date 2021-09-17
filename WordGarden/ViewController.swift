69fc4eb16299b238bd2fa26bb2ebd5d85bea5b47
Mohsin Braer <hbraer@gmail.com>
2021-09-17 19:06:17 -0400

    Section 3.7 complete

--- WordGarden/ViewController.swift
+++ WordGarden/ViewController.swift
@@ -26,25 +26,96 @@ class ViewController: UIViewController {
     @IBOutlet weak var flowerImageView: UIImageView!
     
     
+    var wordsToGuess = ["SWIFT", "DOG", "CAT"];
+    var currentWordIndex = 0;
+    var wordToGuess = "";
+    var lettersGuessed = "";
+    let maxNumberOfWrongGuesses = 8;
+    var wrongGuessesRemaining = 8;
+    
+    var wordsGuessedCount = 0;
+    var wordsMissedCount = 0;
+    var guessCount = 0;
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let text = wordGuessText.text!;
         guessLetterButton.isEnabled = !(text.isEmpty);
+        wordToGuess = wordsToGuess[currentWordIndex];
+        wordRevealLabel.text = "_" + String(repeating: " _", count: wordToGuess.count - 1);
+        
+        updateMessageLabels();
+
+    }
+    
+    func formatRevealedWord()
+    {
+        var revealWord = ""
+        
+        for letter in wordToGuess{
+            if lettersGuessed.contains(letter){
+                revealWord = revealWord + "\(letter) ";
+            } else{
+                revealWord = revealWord + "_ ";
+            }
+            
+        }
+        revealWord.removeLast();
+        wordRevealLabel.text = revealWord;
+    }
+    
+    func guessALetter(){
+        let currentLetterGuessed = wordGuessText.text!
+        lettersGuessed = lettersGuessed + currentLetterGuessed;
+        
+        formatRevealedWord();
+        
+        if(wordToGuess.contains(currentLetterGuessed) == false){
+            wrongGuessesRemaining = wrongGuessesRemaining - 1;
+            flowerImageView.image = UIImage(named: "flower\(wrongGuessesRemaining)");
+            
+        }
+        
+        //update labels
+        guessCount += 1;
+        let guesses = ( guessCount == 1 ? "Guess" : "Guesses");
+      
+        numberGuessesLabel.text = "You've Made \(guessCount) \(guesses)";
+        
+        
+        if (!wordRevealLabel.text!.contains("_"))
+        {
+            numberGuessesLabel.text = "You've guessed it! It took you \(guessCount) guesses to guess the word";
+            wordsGuessedCount += 1;
+            updateAfterWinOrLose();
+        } else if wrongGuessesRemaining == 0{
+            
+            numberGuessesLabel.text = "Game Over! You're all out of guesses";
+            wordsMissedCount -= 1;
+            updateAfterWinOrLose();
+        }
+        
+        
+        if(currentWordIndex == wordToGuess.count){
+            numberGuessesLabel.text = "\n\nYou've tried all of the words! Restart from the beginning?"
+            
+        }
+        
         
         
     }
-    
-    
+
     @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
-        let text = wordGuessText.text!;
-        guessLetterButton.isEnabled = !(text.isEmpty);
-        wordGuessText.text = String(text.last ?? " ").trimmingCharacters(in: .whitespaces);
+        guessLetterButton.isEnabled = !(sender.text!.isEmpty);
+        wordGuessText.text = String(sender.text?.last ?? " ").trimmingCharacters(in: .whitespaces);
         
     }
     
     
     @IBAction func doneKeyPressed(_ sender: UITextField) {
                 
+        guessALetter();
         updateUIAfterGuess();
         
     }
@@ -52,6 +123,7 @@ class ViewController: UIViewController {
     
     @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
                 
+        guessALetter();
         updateUIAfterGuess();
         
     }
@@ -59,7 +131,51 @@ class ViewController: UIViewController {
     
     @IBAction func playAgainButtonPressed(_ sender: UIButton) {
         
+        if currentWordIndex == wordToGuess.count{
+            currentWordIndex = 0;
+            wordsGuessedCount = 0;
+            wordsMissedCount = 0;
+        }
+        
+        playAgainButton.isHidden = true;
+        wordGuessText.isEnabled = true;
+        guessLetterButton.isEnabled = false;
+        wordToGuess = wordsToGuess[currentWordIndex];
+        wrongGuessesRemaining = maxNumberOfWrongGuesses;
+        wordRevealLabel.text = "_" + String(repeating: " _", count: wordsToGuess.count - 1);
+        guessCount = 0;
+        flowerImageView.image = UIImage(named: "flower\(maxNumberOfWrongGuesses)");
+        lettersGuessed = "";
+        updateMessageLabels();
+        numberGuessesLabel.text = "You've Made Zero Guesses";
+        
+
+        
     }
+    
+    
+    func updateMessageLabels()
+    {
+        wordsGuessedLabel.text = "Words Guessed: \(wordsGuessedCount)";
+        wordsMissedLabel.text = "Words Missed: \(wordsMissedCount)";
+        wordsRemainingLabel.text = "Words to Guess: \(wordsToGuess.count - (wordsMissedCount + wordsGuessedCount))";
+        wordsInGameLabel.text = "Words in Game: \(wordsToGuess.count)";
+    }
+    
+    
+    func updateAfterWinOrLose(){
+        
+        currentWordIndex += 1;
+        wordGuessText.isEnabled = false;
+        guessLetterButton.isEnabled = false;
+        playAgainButton.isHidden = false;
+        
+        updateMessageLabels();
+        
+
+        
+    }
+    
     
     func updateUIAfterGuess() {
         wordGuessText.resignFirstResponder();
